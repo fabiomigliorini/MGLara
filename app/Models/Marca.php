61441252a29dd26e6dffcfcfab0faa73c02c7f5b
Fabio Migliorini <fabio.migliorini@gmail.com>
2016-03-04 18:06:21 -0400

    [UPD] Alterada Listagem Marca para Ordem Alfabética

--- app/Models/Marca.php
+++ app/Models/Marca.php
@@ -17,7 +17,7 @@ class Marca extends MGModel
     
     public function ProdutoS()
     {
-        return $this->hasMany(Produto::class, 'codmarca', 'codmarca');
+        return $this->hasMany(Produto::class, 'codmarca', 'codmarca')->orderBy('produto');
     }      
     
     public function validate() {
@@ -39,7 +39,7 @@ class Marca extends MGModel
     public static function filterAndPaginate($marca)
     {
         return Marca::marca($marca)
-            ->orderBy('codmarca', 'DESC')
+            ->orderBy('marca', 'ASC')
             ->paginate(20);
     }
     
