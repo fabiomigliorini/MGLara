ea28ea407a64ff49ddca5b0d31f463f1c9f0f2c9
Luciano Reis <lucianoreis.dev@gmail.com>
2016-03-26 09:06:54 -0400

    [UPD] Relacionamentos das imagens imagem.show

--- resources/views/imagem/show.blade.php
+++ resources/views/imagem/show.blade.php
@@ -6,6 +6,12 @@
             <li>
                 <a href="{{ url('imagem') }}"><span class="glyphicon glyphicon-list-alt"></span> Listagem</a>
             </li> 
+            <li>
+                {!! Form::open(['method' => 'DELETE', 'id'=>'deleteId', 'route' => ['imagem.destroy', $model->codimagem]]) !!}
+                <span class="glyphicon glyphicon-trash"></span>
+                {!! Form::submit('Excluir') !!}
+                {!! Form::close() !!}
+            </li>             
         </ul>
     </div>
 </nav>
@@ -14,11 +20,27 @@
 <hr>
 <div>
     <div class="col-xs-6">
-        <h3>Relacionamentos</h3>
-        <ul>
-            <li>...</li>
-            <li>...</li>
-        </ul>
+        <h3>Relacionamentos</h3>  
+        @foreach($model->GrupoProdutoS as $grupo)
+        <p>
+            <a href="{{ url("grupo-produto/{$grupo->codgrupoproduto}") }}">{{ $grupo->grupoproduto }}</a>
+        </p>
+        @endforeach
+        @foreach($model->MarcaS as $marca)
+        <p>
+            <a href="{{ url("marca/{$marca->codmarca}") }}">{{ $marca->marca }}</a>
+        </p>
+        @endforeach
+        @foreach($model->ProdutoS as $produto)
+        <p>
+            <a href="{{ url("produto/{$produto->codproduto}") }}">{{ $produto->produto }}</a>
+        </p>
+        @endforeach
+        @foreach($model->SubGrupoProdutoS as $subgrupo)
+        <p>
+            <a href="{{ url("sub-grupo-produto/{$subgrupo->codsubgrupoproduto}") }}">{{ $subgrupo->subgrupoproduto }}</a>
+        </p>
+        @endforeach
     </div>
     <div>
         <img src="<?php echo URL::asset('public/imagens/'.$model->observacoes);?>">
