2b36851d1bf1c635013db2275d348696e8dfbad0
Jack Makiyama <jack@phpbr.org>
2016-06-07 14:51:47 -0400

    WIP: templating

--- app/Http/Controllers/NegociosController.php
+++ app/Http/Controllers/NegociosController.php
@@ -2,12 +2,16 @@
 
 namespace MGLara\Http\Controllers;
 
+use Illuminate\Http\Request;
 use MGLara\Http\Controllers\Controller;
+use MGLara\Models\Negocio;
 
 class NegociosController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return view('negocios.index');
+        $model = Negocio::paginate(200);
+
+        return view('negocios.index', compact('model'));
     }
 }
--- resources/views/negocios/index.blade.php
+++ resources/views/negocios/index.blade.php
@@ -14,47 +14,71 @@
 <hr>
 
 
-<form class="form-inline">
-  <div class="form-group">
-    <input type="email" class="form-control" id="id" name="id" placeholder="#">
-  </div>
-  <div class="input-group">
-    <span class="input-group-addon">De</span>
-    <input type="text" class="form-control" id="de" name="de">
-  </div>
-  <div class="input-group">
-    <span class="input-group-addon">Até</span>
-    <input type="text" class="form-control" id="ate" name="ate">
-  </div>
-    <select placeholder="Natureza" class="form-control select-search input-large" name="natureza" id="natureza">
-        <option value=""></option>
-        <option value="0">Devolução de Compra</option>
-    </select>
-    <select placeholder="Pessoa" class="form-control select-search input-xlarge" name="pessoa" id="pessoa">
-        <option value=""></option>
-        <option value="0">Consumidor</option>
-    </select>
-    <select placeholder="Filial" class="form-control select-search input-medium" name="filial" id="filial">
-        <option value=""></option>
-        <option value="0">Deposito</option>
-    </select>
-    <select placeholder="Usuário" class="form-control select-search input-medium" name="usuario" id="usuario">
-        <option value=""></option>
-        <option value="0">Steve Jobs</option>
-    </select>
-    <select placeholder="Status" class="form-control select-search input-medium" name="status" id="status">
-        <option value=""></option>
-        <option value="0">Aberto</option>
-    </select>
-    <select placeholder="Pagamento" class="form-control select-search input-medium" name="pagamento" id="pagamento">
-        <option value=""></option>
-        <option value="0">A Vista</option>
-    </select>
+<form>
+    <div class="row">
+        <div class="form-group col-sm-2">
+            <input type="email" class="form-control" id="id" name="id" placeholder="#">
+        </div>
+        <div class="col-sm-5 row">
+            <div class="col-sm-6">
+                <div class="input-group">
+                    <span class="input-group-addon">De</span>
+                    <input type="text" class="form-control" id="de" name="de">
+                </div>
+            </div>
+            <div class="col-sm-6">
+                <div class="input-group">
+                    <span class="input-group-addon">Até</span>
+                <input type="text" class="form-control" id="ate" name="ate">
+                </div>
+            </div>
+        </div>
+        <div class="form-group col-sm-3">
+            <select placeholder="Natureza" class="form-control select-search" name="natureza" id="natureza">
+                <option value=""></option>
+                <option value="0">Devolução de Compra</option>
+            </select>
+        </div>
+        <div class="form-group col-sm-2">
+            <select placeholder="Filial" class="form-control select-search" name="filial" id="filial">
+                <option value=""></option>
+                <option value="0">Deposito</option>
+            </select>
+        </div>
+    </div>
 
-  <button type="submit" class="btn btn-default">Buscar</button>
+    <div class="row">
+        <div class="form-group col-sm-5">
+            <select placeholder="Pessoa" class="form-control select-search" name="pessoa" id="pessoa">
+            <option value=""></option>
+            <option value="0">Consumidor</option>
+            </select>
+        </div>
+        <div class="col-sm-6 row">
+            <div class="form-group col-sm-4">
+                <select placeholder="Usuário" class="form-control select-search" name="usuario" id="usuario">
+                <option value=""></option>
+                <option value="0">Steve Jobs</option>
+                </select>
+            </div>
+            <div class="form-group col-sm-4">
+                <select placeholder="Status" class="form-control select-search" name="status" id="status">
+                <option value=""></option>
+                <option value="0">Aberto</option>
+                </select>
+            </div>
+            <div class="form-group col-sm-4">
+                <select placeholder="Pagamento" class="form-control select-search" name="pagamento" id="pagamento">
+                <option value=""></option>
+                <option value="0">A Vista</option>
+                </select>
+            </div>
+        </div>
+        <div class="form-group col-sm-1">
+            <button type="submit" class="col-sm-12 btn btn-default">Buscar</button>
+        </div>
+    </div>
 </form>
-
-<hr>
 
 
 <div class="panel panel-default">
@@ -73,19 +97,19 @@
             </tr>
         </tbody>
         <tbody>
-            @for ($i = 0; $i < 10; $i++)
+            @foreach($model as $row)
             <tr>
-                <td>#00411920</td>
-                <td>31/05/2016 13:22:25</td>
-                <td>Venda</td>
-                <td>6,00</td>
-                <td>Consumidor</td>
-                <td><small>Deposito</small></td>
-                <td><small>Steve Jobs</small></td>
-                <td><small>Aberto</small></td>
-                <td><small>Cliente</small></td>
+                <td>{{ formataCodigo($row->codnegocio) }}</td>
+                <td>{{ formataData($row->lancamento, 'L') }}</td>
+                <td>{{ $row->NaturezaOperacao->naturezaoperacao }}</td>
+                <td>{{ formataNumero($row->valortotal) }}</td>
+                <td>{{ $row->Pessoa->fantasia }}</td>
+                <td><small>{{ $row->Filial->filial }}</small></td>
+                <td><small>{{ $row->Usuario->usuario }}</small></td>
+                <td><small>$row->NegocioStatus->negociostatus</small></td>
+                <td><small>$row->PessoaVendedor->fantasia</small></td>
             </tr>
-            @endfor
+            @endforeach
         </tbody>
     </table>
 </div>
