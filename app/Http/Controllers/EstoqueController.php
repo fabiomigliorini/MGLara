f4b253a9e1462cacb2518f9fcb0afff6fd694e50
Fabio Migliorini <fabio@mgpapelaria.com.br>
2016-07-08 10:35:32 -0400

    [UPD] Importacao Conferencia saldo estoque a partir de um Negocio

--- app/Http/Controllers/EstoqueController.php
+++ app/Http/Controllers/EstoqueController.php
@@ -77,30 +77,36 @@ class EstoqueController extends Controller
     
     public function geraSaldoConferenciaNegocio(Request $request, $id)
     {
-        $sql = "select  
-                        pb.codprodutovariacao
-                      , n.codestoquelocal
-                      , sum(npb.quantidade) as quantidadeinformada
-                      , min(npb.alteracao) as alteracao
-                      , min(npb.codusuarioalteracao) as codusuarioalteracao
-                      , min(npb.criacao) as criacao
-                      , min(npb.codusuariocriacao) as codusuariocriacao
-              from tblnegocioprodutobarra npb
-              left join tblnegocio n on (n.codnegocio = npb.codnegocio)
-              left join tblprodutobarra pb on (pb.codprodutobarra = npb.codprodutobarra)
-              where npb.codnegocio = $id
-              group by 
-                        pb.codprodutovariacao
-                      , n.codestoquelocal
-
-              ";
         
-        DB::beginTransaction();
+        $obs = "Criado a partir do Negocio " . formataCodigo($id);
+        
+        if ($esc = EstoqueSaldoConferencia::where('observacoes', $obs)->first())
+            die('Já foi criada conferencia de estoque deste negocio!');
         
         try {
             
+            $sql = "select  
+                            pb.codprodutovariacao
+                          , n.codestoquelocal
+                          , sum(npb.quantidade) as quantidadeinformada
+                          , min(npb.alteracao) as alteracao
+                          , min(npb.codusuarioalteracao) as codusuarioalteracao
+                          , min(npb.criacao) as criacao
+                          , min(npb.codusuariocriacao) as codusuariocriacao
+                  from tblnegocioprodutobarra npb
+                  left join tblnegocio n on (n.codnegocio = npb.codnegocio)
+                  left join tblprodutobarra pb on (pb.codprodutobarra = npb.codprodutobarra)
+                  where npb.codnegocio = $id
+                  group by 
+                            pb.codprodutovariacao
+                          , n.codestoquelocal
+
+                  ";
+
             $regs = DB::select($sql);
 
+            DB::beginTransaction();
+            
             foreach ($regs as $reg)
             {
                 $es = EstoqueSaldo::buscaOuCria($reg->codprodutovariacao, $reg->codestoquelocal, false);
@@ -112,24 +118,84 @@ class EstoqueController extends Controller
                 $model->quantidadeinformada = $reg->quantidadeinformada;
                 $model->customediosistema = $es->customedio;
 
-                $sql = "select em.entradavalor / em.entradaquantidade as custo
-                        from tblestoquemovimento em
-                        inner join tblestoquemes mes on (mes.codestoquemes = em.codestoquemes)
-                        inner join tblestoquesaldo es on (es.codestoquesaldo = mes.codestoquesaldo and es.fiscal = false)
-                        inner join tblestoquelocalprodutovariacao elpv on (elpv.codestoquelocalprodutovariacao = es.codestoquelocalprodutovariacao)
-                        inner join tblprodutovariacao pv on (pv.codprodutovariacao = elpv.codprodutovariacao)
-                        where em.codestoquemovimentotipo = 2001 -- COMPRA
-                        and pv.codproduto = {$es->EstoqueLocalProdutoVariacao->ProdutoVariacao->codproduto}
-                        and coalesce(em.entradaquantidade, 0) > 0
-                        order by data desc
-                        limit 1
-                    ";
+                $model->customedioinformado = $es->customedio;
+                
+                // Tenta custo Medio pela ultima compra do estoque
+                if (empty($model->customedioinformado))
+                {
+                    $sql = "select em.entradavalor / em.entradaquantidade as custo
+                            from tblestoquemovimento em
+                            inner join tblestoquemes mes on (mes.codestoquemes = em.codestoquemes)
+                            inner join tblestoquesaldo es on (es.codestoquesaldo = mes.codestoquesaldo and es.fiscal = false)
+                            inner join tblestoquelocalprodutovariacao elpv on (elpv.codestoquelocalprodutovariacao = es.codestoquelocalprodutovariacao)
+                            inner join tblprodutovariacao pv on (pv.codprodutovariacao = elpv.codprodutovariacao)
+                            where em.codestoquemovimentotipo = 2001 -- COMPRA
+                            and pv.codproduto = {$es->EstoqueLocalProdutoVariacao->ProdutoVariacao->codproduto}
+                            and coalesce(em.entradaquantidade, 0) > 0
+                            order by data desc
+                            limit 1
+                        ";
 
-                if ($custo = DB::select($sql))
-                    $model->customedioinformado = $custo[0]->custo;
+                    if ($custo = DB::select($sql))
+                        $model->customedioinformado = $custo[0]->custo;
+                }
+                
+                //Tenta pela media do custo medio fisico
+                if (empty($model->customedioinformado))
+                {
+                    $sql = "select avg(es.customedio) as custo
+                            from tblprodutovariacao pv
+                            inner join tblestoquelocalprodutovariacao elpv on (elpv.codprodutovariacao = pv.codprodutovariacao)
+                            inner join tblestoquesaldo es on (es.codestoquelocalprodutovariacao = elpv.codestoquelocalprodutovariacao and es.fiscal = false)
+                            where pv.codproduto = {$es->EstoqueLocalProdutoVariacao->ProdutoVariacao->codproduto}
+                            and es.customedio is not null
+                            and es.customedio > 0
+                        ";
+                            
+                    if ($custo = DB::select($sql))
+                        $model->customedioinformado = $custo[0]->custo;
+                }
 
+                //Tenta pela media do custo medio fisico
+                if (empty($model->customedioinformado))
+                {
+                    $sql = "select avg(es.customedio) as custo
+                            from tblprodutovariacao pv
+                            inner join tblestoquelocalprodutovariacao elpv on (elpv.codprodutovariacao = pv.codprodutovariacao)
+                            inner join tblestoquesaldo es on (es.codestoquelocalprodutovariacao = elpv.codestoquelocalprodutovariacao and es.fiscal = true)
+                            where pv.codproduto = {$es->EstoqueLocalProdutoVariacao->ProdutoVariacao->codproduto}
+                            and es.customedio is not null
+                            and es.customedio > 0
+                        ";
+
+                    if ($custo = DB::select($sql))
+                        $model->customedioinformado = $custo[0]->custo;
+                }
+                
+                //Tenta pela ultima nota de compra
+                if (empty($model->customedioinformado))
+                {
+                    $sql = "select (((nfpb.valortotal + nfpb.icmsstvalor + nfpb.ipivalor) * (1- (nf.valordesconto / nf.valorprodutos))) / nfpb.quantidade) / coalesce(pe.quantidade, 1) as custo, nf.codnotafiscal
+                            from tblnotafiscal nf
+                            inner join tblnotafiscalprodutobarra nfpb on (nfpb.codnotafiscal = nf.codnotafiscal)
+                            inner join tblprodutobarra pb on (pb.codprodutobarra = nfpb.codprodutobarra)
+                            inner join tblprodutovariacao pv on (pv.codprodutovariacao = pb.codprodutovariacao)
+                            left join tblprodutoembalagem pe on (pe.codprodutoembalagem = pb.codprodutoembalagem)
+                            where nf.codnaturezaoperacao = 4
+                            and pv.codproduto = {$es->EstoqueLocalProdutoVariacao->ProdutoVariacao->codproduto}
+                            order by nf.saida desc
+                            limit 1
+                        ";
+                            
+                    if ($custo = DB::select($sql))
+                        $model->customedioinformado = $custo[0]->custo;
+                }
+                
+                if (empty($model->customedioinformado))
+                    $model->customedioinformado = $es->EstoqueLocalProdutoVariacao->ProdutoVariacao->Produto->preco * 0.571428571; // 75% markup
+                
                 $model->data = new Carbon('2016-04-01');
-                $model->observacoes = "Criado a partir do Negocio " . formataCodigo($id);
+                $model->observacoes = $obs;
 
                 if (!$model->save())
                     throw new Exception ('Erro ao Salvar EstoqueSaldoConferencia');
@@ -138,11 +204,7 @@ class EstoqueController extends Controller
                 if (!$model->EstoqueSaldo->save())
                     throw new Exception ('Erro ao Salvar EstoqueSaldo');
                 
-                DB::commit();
-
-                $this->dispatch((new EstoqueGeraMovimentoConferencia($model->codestoquesaldoconferencia))->onQueue('urgent'));
-                
-                $gerado[] = [
+                $gerados[] = [
                     'codestoquesaldoconferencia' => $model->codestoquesaldoconferencia,
                     'codestoquesaldo' => $model->codestoquesaldo,
                     'codproduto' => $es->EstoqueLocalProdutoVariacao->ProdutoVariacao->codproduto,
@@ -154,30 +216,19 @@ class EstoqueController extends Controller
 
             }
             
-            return response()->json(['response' => 'Gerado', 'registros' => $gerado]);
+            DB::commit();
+            
+            foreach ($gerados as $reg)
+            {
+                $this->dispatch((new EstoqueGeraMovimentoConferencia($reg["codestoquesaldoconferencia"]))->onQueue('urgent'));
+            }
+            
+            return response()->json(['response' => 'Gerado', 'registros' => $gerados]);
             
         } catch (Exception $exc) {
             DB:rollback();
             dd($exc);
         }
 
-
-
-        
-        /*
-        $this->converteNumericos([
-            'estoquemaximo' => $request->input('estoquemaximo'),
-            'estoqueminimo' => $request->input('estoqueminimo'),
-            'corredor' => $request->input('corredor'),
-            'prateleira' => $request->input('prateleira'),
-            'coluna' => $request->input('coluna'),
-            'bloco' => $request->input('bloco'),
-            'quantidadeinformada' => $request->input('quantidadeinformada'),
-            'customedioinformado' => $request->input('customedioinformado'),
-        ]);
-        
-        //$this->converteDatas(['data' => $request->input('data')]);
-        
-        */
     }
 }
