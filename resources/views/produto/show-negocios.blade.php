943e4b67ba11ea1d3482959a228b9248b5b54360
Luciano Reis <lucianoreis.dev@gmail.com>
2016-07-12 09:24:22 -0400

    [UPD] Correção enchimento listgem de negócio do Produto

--- resources/views/produto/show-negocios.blade.php
+++ resources/views/produto/show-negocios.blade.php
@@ -16,45 +16,45 @@
                         <small>
                         <small>
                             <div class='col-sm-2 '>
-                                <div class='col-sm-12'>
+                                <div>
                                     <a href="{{ url('negocio', ['id'=>$npb->codnegocio]) }}">
                                         {{ formataCodigo($npb->codnegocio) }}
                                     </a>
                                 </div>
-                                <div class='col-sm-12 text-muted'>
+                                <div class='text-muted'>
                                     {{ formataData($npb->Negocio->lancamento) }}
                                 </div>
                             </div>
                             <div class='col-sm-4'>
-                                <div class='col-sm-12'>
+                                <div class=''>
                                     <a href='{{ url('pessoa', ['id'=>$npb->Negocio->codpessoa]) }}'>
                                         {{ $npb->Negocio->Pessoa->fantasia }}
                                     </a>
                                 </div>
-                                <div class='col-sm-12 text-muted'>
+                                <div class='text-muted'>
                                     <a href='{{ url('natureza-operacao', ['id'=>$npb->Negocio->codnaturezaoperacao]) }}'>
                                         {{ $npb->Negocio->NaturezaOperacao->naturezaoperacao }}
                                     </a>
                                 </div>
                             </div>
                             <div class='col-sm-3'>
-                                <div class='col-sm-12 text-muted'>
+                                <div class='text-muted'>
                                     <a href='{{ url('filial', ['id'=>$npb->Negocio->codfilial]) }}'>
                                         {{ $npb->Negocio->Filial->filial }}
                                     </a>
                                 </div>
-                                <div class='col-sm-12'>
+                                <div>
                                     {{ $npb->ProdutoBarra->ProdutoVariacao->variacao }}
                                 </div>
-                                <div class='col-sm-12 text-muted'>
+                                <div class='text-muted'>
                                     {{ $npb->ProdutoBarra->barras }}
                                 </div>
                             </div>
                             <div class='col-sm-3'>
-                                <div class='col-sm-12 text-right'>
+                                <div class='text-right'>
                                     <small class='pull-left'>R$</small> {{ formataNumero($valor, 2) }} 
                                 </div>
-                                <div class='col-sm-12 text-right text-muted'>
+                                <div class='text-right text-muted'>
                                     <small class='pull-left'>{{ $model->UnidadeMedida->sigla }}</small> {{ formataNumero($quantidade, 3) }} 
                                 </div>
                             </div>
