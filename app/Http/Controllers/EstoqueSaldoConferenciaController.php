231aaeb607d1ebde35d286146bb8e548521d6019
Luciano Reis <lucianoreis.dev@gmail.com>
2016-07-29 16:48:31 -0400

    [UPD] Passando data EstoqueSaldoConferenciaController

--- app/Http/Controllers/EstoqueSaldoConferenciaController.php
+++ app/Http/Controllers/EstoqueSaldoConferenciaController.php
@@ -33,21 +33,36 @@ class EstoqueSaldoConferenciaController extends Controller
      */
     public function index(Request $request)
     {
-        if (!$request->session()->has('estoque-saldo-conferencia.index')) 
+        
+        if (!$request->session()->has('estoque-saldo-conferencia.index')) {
             $request->session()->put('estoque-saldo-conferencia.index');
-        
-        if (!empty($parametros['data_de']))
-            $parametros['data_de'] = new Carbon($parametros['data_de'] . ' 00:00:00');
-        if (!empty($parametros['data_ate']))
-            $parametros['data_ate'] = new Carbon($parametros['data_ate'] . ' 23:59:59');
-        
-        if (!empty($parametros['criacao_de']))
-            $parametros['criacao_de'] = new Carbon($parametros['criacao_de'] . ' 00:00:00');
-        if (!empty($parametros['criacao_ate']))
-            $parametros['criacao_ate'] = new Carbon($parametros['criacao_ate'] . ' 23:59:59');
-        
+        }
+        /*
+        if (!empty($request->session()->get('estoque-saldo-conferencia.index.data_de'))) {
+            $request->session()->put('estoque-saldo-conferencia.index.data_de', new Carbon($request->session()->get('estoque-saldo-conferencia.index.data_de' . ' 00:00:00')));
+        }
+
+        if (!empty($request->session()->get('estoque-saldo-conferencia.index.data_ate'))) {
+            $request->session()->put('estoque-saldo-conferencia.index.data_ate', new Carbon($request->session()->get('estoque-saldo-conferencia.index.data_ate' . ' 23:59:59')));
+        }
+
+        if (!empty($request->session()->get('estoque-saldo-conferencia.index.criacao_de'))) {
+            $data = Carbon::createFromFormat('Y-m-d H', $request->session()->get('estoque-saldo-conferencia.index.criacao_de') . ' 00:00:00')->toDateTimeString();
+            dd($data);
+            //$request->session()->put('estoque-saldo-conferencia.index.criacao_de', new Carbon());
+            $request->session()->put('estoque-saldo-conferencia.index.criacao_de', $data);
+            //$request->session()->put('estoque-saldo-conferencia.index.criacao_de', Carbon::createFromFormat($formato, $value)->toDateTimeString());
+        }
+
+        if (!empty($request->session()->get('estoque-saldo-conferencia.index.criacao_ate'))) {
+            $request->session()->put('estoque-saldo-conferencia.index.criacao_ate', new Carbon($request->session()->get('estoque-saldo-conferencia.index.criacao_ate' . ' 23:59:59')));
+        }
+        */
+
         $parametros = $request->session()->get('estoque-saldo-conferencia.index');
         
+        //dd($parametros);
+
         $model = EstoqueSaldoConferencia::search($parametros)->select('tblestoquesaldoconferencia.*')->orderBy('codestoquesaldoconferencia', 'DESC')->paginate(20);
         return view('estoque-saldo-conferencia.index', compact('model'));
     }
