762e21c2863092bdd6c8642b6b09d2bf5a70507a
Luciano Reis <lucianoreis.dev@gmail.com>
2016-08-10 16:51:30 -0400

    [UPD] Correção layouts formulários de filtros SecaoProduto

--- resources/views/grupo-produto/show.blade.php
+++ resources/views/grupo-produto/show.blade.php
@@ -26,22 +26,6 @@
         </ul>
     </div>
 </nav>
-<div class="pull-right foto-item-unico">
-    @if(empty($model->codimagem))
-        <a class="btn btn-default carregar" href="{{ url("/imagem/edit?id=$model->codgrupoproduto&model=GrupoProduto") }}">
-            <i class="glyphicon glyphicon-picture"></i>
-            Carregar imagem
-        </a>
-    @else
-    <a href="{{ url("imagem/{$model->Imagem->codimagem}") }}">
-        <img class="img-responsive pull-right" src='<?php echo URL::asset('public/imagens/'.$model->Imagem->observacoes);?>'>
-    </a>
-    <span class="caption simple-caption">
-        <a href="{{ url("/imagem/edit?id=$model->codgrupoproduto&model=GrupoProduto") }}" class="btn btn-default btn-sm"><i class="glyphicon glyphicon-pencil"></i> Alterar</a>
-    </span>        
-    @endif
-</div>
-
 <h1 class="header">
 {!! 
     titulo(
@@ -53,32 +37,68 @@
         ],
         $model->inativo
     ) 
-!!} 
+!!}
+    <a class="btn btn-primary pull-right" role="button" data-toggle="collapse" href="#div-filtro" aria-expanded="false" aria-controls="div-filtro">
+        <span class='glyphicon glyphicon-search'></span>
+    </a>   
+    <div class="pull-right foto-item-unico">
+        @if(empty($model->codimagem))
+            <a class="btn btn-default carregar" href="{{ url("/imagem/edit?id=$model->codgrupoproduto&model=GrupoProduto") }}">
+                <i class="glyphicon glyphicon-picture"></i>
+                Carregar imagem
+            </a>
+        @else
+        <a href="{{ url("imagem/{$model->Imagem->codimagem}") }}">
+            <img class="img-responsive pull-right" src='<?php echo URL::asset('public/imagens/'.$model->Imagem->observacoes);?>'>
+        </a>
+        <span class="caption simple-caption">
+            <a href="{{ url("/imagem/edit?id=$model->codgrupoproduto&model=GrupoProduto") }}" class="btn btn-default btn-sm"><i class="glyphicon glyphicon-pencil"></i> Alterar</a>
+        </span>        
+        @endif
+    </div>
 </h1>
 @include('includes.autor')
-<hr>
-{!! Form::model(
-    Request::session()->get('grupo-produto.show'),
-    [
-        'route' => 'grupo-produto.show', 
-        'method' => 'GET', 
-        'class' => 'form-inline', 
-        'id' => 'sub-grupo-produto-search', 
-        'role' => 'search', 
-        'autocomplete' => 'off'
-    ]
-)!!}
+<div class="clearfix"></div>
+<div class='collapse' id='div-filtro'>
+    <div class='well well-sm' style="padding:9px 0">
+        {!! Form::model(
+            Request::session()->get('grupo-produto.show'),
+            [
+                'route' => 'grupo-produto.show', 
+                'method' => 'GET', 
+                'class' => 'form-horizontal', 
+                'id' => 'sub-grupo-produto-search', 
+                'role' => 'search', 
+                'autocomplete' => 'off'
+            ]
+        )!!}
+        <div class="col-md-4">
+            <div class="form-group">
+                {!! Form::label('subgrupoproduto', 'Sub Grupo', ['class' => 'col-sm-3 control-label']) !!}
+                <div class="col-md-9">{!! Form::text('subgrupoproduto', null, ['class' => 'form-control', 'placeholder' => 'Sub Grupo']) !!}</div>
+            </div>
+        </div>
 
-    <div class="form-group">
-        {!! Form::text('subgrupoproduto', null, ['class' => 'form-control', 'placeholder' => 'Sub Grupo']) !!}
+        <div class="col-md-2">
+            <div class="form-group">
+                {!! Form::label('ativo', 'Ativo', ['class' => 'col-sm-3 control-label']) !!}
+                <div class="col-md-9">{!! Form::select2Ativo('ativo', null, ['class'=> 'form-control', 'id' => 'ativo']) !!}</div>
+            </div>
+        </div>
+
+        <div class="col-md-3">   
+             <div class="form-group">
+                <div class="col-md-12">   
+                    <button type="submit" class="btn btn-default"><i class=" glyphicon glyphicon-search"></i> Buscar</button>
+                    <a class="btn btn-default" href="{{ url("sub-grupo-produto/create?codgrupoproduto=$model->codgrupoproduto") }}"><i class=" glyphicon glyphicon-plus"></i> Novo Sub Grupo</a>
+                </div>
+            </div>
+        </div>
+        {!! Form::close() !!}
+        <div class="clearfix"></div>
     </div>
-    <div class="form-group">
-        {!! Form::select2Ativo('ativo', null, ['class'=> 'form-control', 'id' => 'ativo', 'style'=>'width:120px']) !!}
-    </div>      
-    <button type="submit" class="btn btn-default"><i class=" glyphicon glyphicon-search"></i> Buscar</button>
-    <a class="btn btn-default" href="{{ url("sub-grupo-produto/create?codgrupoproduto=$model->codgrupoproduto") }}"><i class=" glyphicon glyphicon-plus"></i> Novo Sub Grupo</a>
-{!! Form::close() !!}
-<br>
+    {!! Form::close() !!}
+</div>
 <div id="registros">
   <div class="list-group list-group-striped list-group-hover" id="items">
     @foreach($subgrupos as $row)
--- resources/views/sub-grupo-produto/show.blade.php
+++ resources/views/sub-grupo-produto/show.blade.php
@@ -26,59 +26,79 @@
         </ul>
     </div>
 </nav>
-<div class="pull-right foto-item-unico">
-    @if(empty($model->codimagem))
-        <a class="btn btn-default carregar" href="{{ url("/imagem/edit?id=$model->codsubgrupoproduto&model=SubGrupoProduto") }}">
-            <i class="glyphicon glyphicon-picture"></i>
-            Carregar imagem
-        </a>
-    @else
-    <a href="{{ url("imagem/{$model->Imagem->codimagem}") }}">
-        <img class="img-responsive pull-right" src='<?php echo URL::asset('public/imagens/'.$model->Imagem->observacoes);?>'>
-    </a>
-    <span class="caption simple-caption">
-        <a href="{{ url("/imagem/edit?id=$model->codsubgrupoproduto&model=SubGrupoProduto") }}" class="btn btn-default btn-sm"><i class="glyphicon glyphicon-pencil"></i> Alterar</a>
-    </span>        
-    @endif
-</div>
-
 <h1 class="header">
-{!! 
-    titulo(
-        $model->codgrupoproduto,
-        [
-            url("secao-produto/{$model->GrupoProduto->FamiliaProduto->SecaoProduto->codsecaoproduto}") => $model->GrupoProduto->FamiliaProduto->SecaoProduto->secaoproduto,
-            url("familia-produto/{$model->GrupoProduto->FamiliaProduto->codfamiliaproduto}") => $model->GrupoProduto->FamiliaProduto->familiaproduto,
-            url("grupo-produto/$model->codgrupoproduto") => $model->GrupoProduto->grupoproduto,
-            $model->subgrupoproduto,
-        ],
-        $model->inativo
-    ) 
-!!} 
+    {!! 
+        titulo(
+            $model->codgrupoproduto,
+            [
+                url("secao-produto/{$model->GrupoProduto->FamiliaProduto->SecaoProduto->codsecaoproduto}") => $model->GrupoProduto->FamiliaProduto->SecaoProduto->secaoproduto,
+                url("familia-produto/{$model->GrupoProduto->FamiliaProduto->codfamiliaproduto}") => $model->GrupoProduto->FamiliaProduto->familiaproduto,
+                url("grupo-produto/$model->codgrupoproduto") => $model->GrupoProduto->grupoproduto,
+                $model->subgrupoproduto,
+            ],
+            $model->inativo
+        ) 
+    !!} 
+    <a class="btn btn-primary pull-right" role="button" data-toggle="collapse" href="#div-filtro" aria-expanded="false" aria-controls="div-filtro">
+        <span class='glyphicon glyphicon-search'></span>
+    </a> 
+    <div class="pull-right foto-item-unico">
+        @if(empty($model->codimagem))
+            <a class="btn btn-default carregar" href="{{ url("/imagem/edit?id=$model->codsubgrupoproduto&model=SubGrupoProduto") }}">
+                <i class="glyphicon glyphicon-picture"></i>
+                Carregar imagem
+            </a>
+        @else
+        <a href="{{ url("imagem/{$model->Imagem->codimagem}") }}">
+            <img class="img-responsive pull-right" src='<?php echo URL::asset('public/imagens/'.$model->Imagem->observacoes);?>'>
+        </a>
+        <span class="caption simple-caption">
+            <a href="{{ url("/imagem/edit?id=$model->codsubgrupoproduto&model=SubGrupoProduto") }}" class="btn btn-default btn-sm"><i class="glyphicon glyphicon-pencil"></i> Alterar</a>
+        </span>        
+        @endif
+    </div>
 </h1>
 @include('includes.autor')
-<hr>
-{!! Form::model(
-    Request::session()->get('sub-grupo-produto.show'),
-    [
-        'route' => 'grupo-produto.show', 
-        'method' => 'GET', 
-        'class' => 'form-inline', 
-        'id' => 'produto-search', 
-        'role' => 'search', 
-        'autocomplete' => 'off'
-    ]
-)!!}
-
-    <div class="form-group">
-        {!! Form::text('produto', null, ['class' => 'form-control', 'placeholder' => 'Produto']) !!}
+<div class="clearfix"></div>
+<div class='collapse' id='div-filtro'>
+    <div class='well well-sm' style="padding:9px 0">
+        {!! Form::model(
+            Request::session()->get('sub-grupo-produto.show'),
+            [
+                'route' => 'grupo-produto.show', 
+                'method' => 'GET', 
+                'class' => 'form-horizontal', 
+                'id' => 'produto-search', 
+                'role' => 'search', 
+                'autocomplete' => 'off'
+            ]
+        )!!}
+        <div class="col-md-4">
+            <div class="form-group">
+                {!! Form::label('produto', 'Produto', ['class' => 'col-sm-3 control-label']) !!}
+                <div class="col-md-9">{!! Form::text('produto', null, ['class' => 'form-control', 'placeholder' => 'Produto']) !!}</div>
+            </div>
+        </div>
+        
+        <div class="col-md-3">    
+            <div class="form-group">
+                {!! Form::label('ativo', 'Ativo', ['class' => 'col-sm-3 control-label']) !!}
+                <div class="col-md-9">{!! Form::select2Ativo('ativo', null, ['class'=> 'form-control', 'id' => 'ativo']) !!}</div>
+            </div>
+        </div>
+        
+        <div class="col-md-3">    
+            <div class="form-group">
+                <div class="col-md-12">   
+                    <button type="submit" class="btn btn-default"><i class=" glyphicon glyphicon-search"></i> Buscar</button>
+                </div>
+            </div>
+        </div>
+        {!! Form::close() !!}
+        <div class="clearfix"></div>
     </div>
-    <div class="form-group">
-        {!! Form::select2Ativo('ativo', null, ['class'=> 'form-control', 'id' => 'ativo', 'style'=>'width:120px']) !!}
-    </div>          
-    <button type="submit" class="btn btn-default"><i class=" glyphicon glyphicon-search"></i> Buscar</button>
-{!! Form::close() !!}
-<br>
+    {!! Form::close() !!}
+</div>
 <div id="registros">
   <div class="list-group list-group-striped list-group-hover" id="items">
     @foreach($produtos as $row)
