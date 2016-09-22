de9aee523a4957641566b455bfd349b082f0a377
Luciano Reis <lucianoreis.dev@gmail.com>
2016-09-22 07:04:13 -0400

    [UPD] Ajuste tamanho de campos form Usuario

--- resources/views/usuario/form.blade.php
+++ resources/views/usuario/form.blade.php
@@ -21,104 +21,83 @@
             $printers[$model->impressoratelanegocio] = $model->impressoratelanegocio;
 ?>
 <div class="form-group">
-  <label for="usuario" class="col-sm-2 control-label">
-  	{!! Form::label('Usuário', 'Usuário:') !!}
-  </label>
-  <div class="col-md-2 col-xs-4">
-  	{!! Form::text('usuario', null, ['class'=> 'form-control', 'id'=>'usuario', 'required'=>'required']) !!}
-  </div>
+    {!! Form::label('usuario', 'Usuário', ['class' => 'col-sm-2 control-label']) !!}
+    <div class="col-md-2 col-xs-4">
+        {!! Form::text('usuario', null, ['class'=> 'form-control', 'id'=>'usuario', 'required'=>'required']) !!}
+    </div>
 </div>
 
 <div class="form-group">
-  <label for="senha" class="col-sm-2 control-label">
-  	{!! Form::label('Senha', 'Senha:') !!}
-  </label>
-  <div class="col-md-2 col-xs-4">
-  	{!! Form::password('senha', ['class'=> 'form-control', 'id'=>'senha', isset($model) ? null : 'required=required', 'minlength'=>'6' ]) !!}
-  </div>
+    {!! Form::label('senha', 'Senha', ['class' => 'col-sm-2 control-label']) !!}
+    <div class="col-md-2 col-xs-4">
+        {!! Form::password('senha', ['class'=> 'form-control', 'id'=>'senha', isset($model) ? null : 'required=required', 'minlength'=>'6' ]) !!}
+    </div>
 </div>
 
 <div class="form-group">
-  <label for="senha" class="col-sm-2 control-label">
-  	{!! Form::label('Confirmação', 'Confirmação:') !!}
-  </label>
-  <div class="col-md-2 col-xs-4">
-  	{!! Form::password('repetir_senha', ['class'=> 'form-control', 'id'=>'repetir_senha', isset($model) ? null : 'required=required']) !!}
+    {!! Form::label('repetir_senha', 'Confirmação', ['class' => 'col-sm-2 control-label']) !!}
+    <div class="col-md-2 col-xs-4">
+        {!! Form::password('repetir_senha', ['class'=> 'form-control', 'id'=>'repetir_senha', isset($model) ? null : 'required=required']) !!}
         <span id="error-rpt"></span>
-  </div>
+    </div>
 </div>
 
 <div class="form-group">
-  <label for="codecf" class="col-sm-2 control-label">
-  	{!! Form::label('ECF', 'ECF:') !!}
-  </label>
-  <div class="col-md-2 col-xs-4">
+    {!! Form::label('codecf', 'ECF', ['class' => 'col-sm-2 control-label']) !!}
+    <div class="col-md-2 col-xs-4">
         {!! Form::select2Ecf('codecf', null, ['class' => 'form-control', 'id'=>'codecf']) !!}
-  </div>
+    </div>
 </div>
 
 <div class="form-group">
-  <label for="codfilial" class="col-sm-2 control-label">
-  	{!! Form::label('Filial', 'Filial:') !!}
-  </label>
-  <div class="col-md-2 col-xs-4">
+    {!! Form::label('codfilial', 'Filial', ['class' => 'col-sm-2 control-label']) !!}
+    <div class="col-md-2 col-xs-4">
         {!! Form::select2Filial('codfilial', null, ['class' => 'form-control', 'id'=>'codfilial']) !!}
-  </div>
+    </div>
 </div>
 
 <div class="form-group">
-  <label for="codoperacao" class="col-sm-2 control-label">
-  	{!! Form::label('Operação', 'Operação:') !!}
-  </label>
-  <div class="col-md-2 col-xs-4">
+    {!! Form::label('codoperacao', 'Operação', ['class' => 'col-sm-2 control-label']) !!}
+    <div class="col-md-2 col-xs-4">
         {!! Form::select2Operacao('codoperacao', null, ['class' => 'form-control', 'id'=>'codoperacao']) !!}
-  </div>
+    </div>
 </div>
 
 <div class="form-group">
-  <label for="codpessoa" class="col-sm-2 control-label">
-  	{!! Form::label('Pessoa', 'Pesso:') !!}
-  </label>
+    {!! Form::label('codpessoa', 'Pessoa', ['class' => 'col-sm-2 control-label']) !!}
   <div class="col-sm-4">
         {!! Form::select2Pessoa('codpessoa', null, ['class' => 'form-control', 'id'=>'codpessoa', 'placeholder' => 'Pessoa', 'ativo' => 1]) !!}
   </div>
 </div>
 
 <div class="form-group">
-  <label for="impressoramatricial" class="col-sm-2 control-label">
-  	{!! Form::label('Impressora Matricial', 'Impressora Matricial:') !!}
-  </label>
-  <div class="col-sm-3">
-  	{!! Form::select('impressoramatricial', $printers, ['class'=> 'form-control'], ['id'=>'impressoramatricial','required'=>'required']) !!}
-  </div>
+    {!! Form::label('impressoramatricial', 'Impressora Matricial', ['class' => 'col-sm-2 control-label']) !!}  
+    <div class="col-sm-3">
+        {!! Form::select2('impressoramatricial', $printers, null, ['class'=> 'form-control', 'id'=>'impressoramatricial','required'=>'required', 'placeholder' => 'Impressora Matricial']) !!}
+    </div>
 </div>
 
 <div class="form-group">
-  <label for="impressoratermica" class="col-sm-2 control-label">
-  	{!! Form::label('Impressora Térmica', 'Impressora Térmica:') !!}
-  </label>
-  <div class="col-sm-3">
-  	{!! Form::select('impressoratermica', $printers, ['class'=> 'form-control'], ['id'=>'impressoratermica','required'=>'required']) !!}
-  </div>
+    {!! Form::label('impressoratermica', 'Impressora Térmica', ['class' => 'col-sm-2 control-label']) !!}  
+    <div class="col-sm-3">
+        {!! Form::select2('impressoratermica', $printers, null, ['class' => 'form-control', 'id'=>'impressoratermica','required'=>'required', 'placeholder' => 'Impressora Termica']) !!}
+    </div>
 </div>
 
+<!--
 <div class="form-group">
-  <label for="impressoratermica" class="col-sm-2 control-label">
-  	{!! Form::label('Impressora tela negócio', 'Impressora tela negócio:') !!}
-  </label>
-  <div class="col-sm-3">
-  	{!! Form::select('impressoratelanegocio', $printers, ['class'=> 'form-control'], ['id'=>'impressoratelanegocio']) !!}
-  </div>
+    {!! Form::label('impressoratelanegocio', 'Impressora tela negócio', ['class' => 'col-sm-2 control-label']) !!}  
+    <div class="col-sm-3">
+        {!! Form::select('impressoratelanegocio', $printers, ['class'=> 'form-control'], ['id'=>'impressoratelanegocio']) !!}
+    </div>
 </div>
+-->
 
 <div class="form-group">
-  <label for="codportador" class="col-sm-2 control-label">
-  	{!! Form::label('Portador', 'Portador:') !!}
-  </label>
-  <div class="col-sm-3">
+    {!! Form::label('codportador', 'Portador', ['class' => 'col-sm-2 control-label']) !!}  
+    <div class="col-sm-3">
         {!! Form::select2Portador('codportador', null, ['class' => 'form-control', 'id'=>'codportador']) !!}
-        
-  </div>
+    </div>
 </div>
 
 <div class="form-group">
@@ -166,25 +145,7 @@ $(document).ready(function() {
             }
         });
     });
-    
-    $('#impressoratermica').select2({
-        allowClear: true,
-        width: 'resolve'
-    })<?php echo (isset($model->impressoratermica) ? ".select2('val', '$model->impressoratermica');" : ';');?>    
-    
-    $('#impressoramatricial').select2({
-        allowClear: true,
-        width: 'resolve'
-    })<?php echo (isset($model->impressoramatricial) ? ".select2('val', '$model->impressoramatricial');" : ';');?>    
 
-    $('#impressoratelanegocio').select2({
-        allowClear: true,
-        width: 'resolve'
-    })<?php echo (isset($model->impressoratelanegocio) ? ".select2('val', '$model->impressoratelanegocio');" : ';');?>    
-    
-    
-
-    
 });
 </script>
 @endsection
