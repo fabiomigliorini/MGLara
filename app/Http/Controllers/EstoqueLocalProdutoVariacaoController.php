cff04a06aa85197ffa1813e3225f28718df88153
Fabio Migliorini <fabio@mgpapelaria.com.br>
2016-10-17 18:44:45 -0300

    [UPD] Calculo Estatistica Estoque

---
+++ app/Http/Controllers/EstoqueLocalProdutoVariacaoController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace MGLara\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use MGLara\Http\Requests;
+use MGLara\Http\Controllers\Controller;
+//use MGLara\Models\EstoqueLocalProdutoVariacao;
+use MGLara\Models\EstoqueLocal;
+use MGLara\Models\Produto;
+
+class EstoqueLocalProdutoVariacaoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $prod = Produto::findOrFail($id);
+        $locais = EstoqueLocal::ativo()->get();
+        return view('estoque-local-produto-variacao.show', compact('prod', 'locais'));
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
+}
