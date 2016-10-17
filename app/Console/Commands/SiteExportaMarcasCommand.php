1947f1fbcaba15521d7120bbedd90931154d4654
Fabio Migliorini <fabio@mgpapelaria.com.br>
2016-10-17 18:43:58 -0300

    [UPD] Exportacao Marcas Para OpenCart

--- app/Console/Commands/SiteExportaMarcasCommand.php
+++ app/Console/Commands/SiteExportaMarcasCommand.php
@@ -10,6 +10,7 @@ use MGLara\Models\Marca;
 
 class SiteExportaMarcasCommand extends Command
 {
+    const URL_SITE = 'http://webapp15505.cloud683.configrapp.com/';
     //use DispatchesJobs;
     
     /**
@@ -35,7 +36,7 @@ class SiteExportaMarcasCommand extends Command
     {
         parent::__construct();
     }
-
+    
     /**
      * Execute the console command.
      *
@@ -49,7 +50,7 @@ class SiteExportaMarcasCommand extends Command
         $chave = base64_encode('mgpapelaria:123456');
         
         // Gera o Token de Autenticacao
-        $ch = curl_init('http://webapp15505.cloud683.configrapp.com/index.php?route=rest/admin_security/gettoken&grant_type=client_credentials');
+        $ch = curl_init(SELF::URL_SITE . 'index.php?route=rest/admin_security/gettoken&grant_type=client_credentials');
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
         curl_setopt($ch, CURLOPT_HEADER, 0);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -75,10 +76,10 @@ class SiteExportaMarcasCommand extends Command
         } 
         
         // Token temporario
-        //$token = '558cb6ad4a66d06a3671e920c8d1de15c5cc91b3';
+        //$token = '13f8ea04e1728a03a1a71f2340b086eba3e5f595';
         
         // Busca Listagem das marcas do OpenCart
-        $ch = curl_init('http://webapp15505.cloud683.configrapp.com/index.php?route=rest/manufacturer_admin/manufacturer&limit=10000000');
+        $ch = curl_init(SELF::URL_SITE . 'index.php?route=rest/manufacturer_admin/manufacturer&limit=10000000');
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
         curl_setopt($ch, CURLOPT_HEADER, 0);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -108,11 +109,12 @@ class SiteExportaMarcasCommand extends Command
         }
         
         // Busca as Marcas do Sistema
+        //$marcas = Marca::whereNotNull('codimagem')->orderBy('codmarca')->limit(10)->get();
         $marcas = Marca::orderBy('codmarca')->get();
         
         // Percorre todas as marcas
         foreach ($marcas as $marca) {
-            
+
             // String com os dados da Marca
             $data_string = json_encode([
                 'name' => $marca->marca,
@@ -125,7 +127,7 @@ class SiteExportaMarcasCommand extends Command
             //Se ja esta no OpenCart - ATUALIZA
             if (isset($marcas_site[$marca->codopencart])) {
                 
-                $ch = curl_init("http://webapp15505.cloud683.configrapp.com/index.php?route=rest/manufacturer_admin/manufacturer&id={$marca->codopencart}");
+                $ch = curl_init(SELF::URL_SITE . "index.php?route=rest/manufacturer_admin/manufacturer&id={$marca->codopencart}");
                 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");   
                 curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);                                                                  
                 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
@@ -146,7 +148,7 @@ class SiteExportaMarcasCommand extends Command
             // Se nao esta no OpenCart - CRIA
             } else {
                 
-                $ch = curl_init("http://webapp15505.cloud683.configrapp.com/index.php?route=rest/manufacturer_admin/manufacturer");
+                $ch = curl_init(SELF::URL_SITE . "index.php?route=rest/manufacturer_admin/manufacturer");
                 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");   
                 curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);                                                                  
                 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
@@ -182,6 +184,89 @@ class SiteExportaMarcasCommand extends Command
 
             }
             
+            // TODO: logica para saber se precisa atualizar a imagem
+            $atualizar_imagem = empty($marca->codimagem)?false:true;
+            
+            if ($atualizar_imagem) {
+                
+                
+                $target = SELF::URL_SITE . "index.php?route=rest/manufacturer_admin/manufacturerimages&id={$marca->codopencart}";
+
+                // Create a CURLFile object / procedural method 
+                $imagePath = base_path('public/imagens/'.$marca->Imagem->observacoes);
+                echo "{$marca->codopencart} - {$marca->marca} - $imagePath\n";
+                $imageExtention = preg_replace('/^.*\.([^.]+)$/D', '$1', $imagePath);
+                $cfile = curl_file_create($imagePath, mime_content_type($imagePath), $imagePath); // try adding 
+                //dd($cfile);
+                // Assign POST data
+                $imgdata = array('file' => $cfile);
+
+                $curl = curl_init();
+                curl_setopt($curl, CURLOPT_URL, $target);
+                //curl_setopt($curl, CURLOPT_USERAGENT,'Opera/9.80 (Windows NT 6.2; Win64; x64) Presto/2.12.388 Version/12.15');
+                //curl_setopt($curl, CURLOPT_HTTPHEADER,array('User-Agent: Opera/9.80 (Windows NT 6.2; Win64; x64) Presto/2.12.388 Version/12.15','Referer: http://someaddress.tld','Content-Type: multipart/form-data'));
+                curl_setopt($curl, CURLOPT_HTTPHEADER, array(                                                                          
+                    "Authorization: Bearer $token"
+                ));
+                curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false); // stop verifying certificate
+                curl_setopt($curl, CURLOPT_RETURNTRANSFER, true); 
+                curl_setopt($curl, CURLOPT_POST, true); // enable posting
+                curl_setopt($curl, CURLOPT_POSTFIELDS, $imgdata); // post images 
+                curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true); // if any redirection after upload
+                $r = curl_exec($curl); 
+                curl_close($curl);
+                
+                //dd($r);
+                /*
+                
+                $postFields['file'] = curl_file_create($imagePath, "image/$imageExtention", 'file');
+                */
+
+                /*
+                //$postFields['file'] = "@$imagePath;type=image/$imageExtention";
+                $postFields['file'] = new \CURLFile($imagePath);
+                dd($postFields['file']);
+                //$x = new \CURLFile($filename)
+
+                //dd(SELF::URL_SITE . "index.php?route=rest/manufacturer_admin/manufacturerimages&id={$marca->codopencart}");
+                */
+                //dd($postFields);
+                
+                /*
+                $ch = curl_init(SELF::URL_SITE . "index.php?route=rest/manufacturer_admin/manufacturerimages&id={$marca->codopencart}");
+                curl_setopt ($ch, CURLOPT_POST, 1);
+                curl_setopt ($ch, CURLOPT_POSTFIELDS, $postFields);
+                curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
+                 * 
+                 */
+                /*
+                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");   
+                curl_setopt($ch, CURLOPT_HEADER, 0);
+                curl_setopt($ch, CURLOPT_SAFE_UPLOAD, true);
+                curl_setopt($ch, CURLOPT_POSTFIELDS, $postFields);                                                                  
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
+                 * 
+                 */
+                /*
+                curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
+                    'Content-Type: application/json',     
+                    "Authorization: Bearer $token"
+                ));
+
+                $response = curl_exec($ch);
+                $status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+                curl_close($ch);
+                dd($response);
+                if ($status != 200) {
+                    echo("ERROR");
+                } else {
+                    echo($response);
+                }
+                 * 
+                 */
+                
+            }            
+            
             // Exclui do array de Marcas do OpenCart
             unset($marcas_site[$marca->codopencart]);
         }
@@ -191,7 +276,7 @@ class SiteExportaMarcasCommand extends Command
             
             $data_string = json_encode(['manufacturers' => [$manufacturer_id]]);
             
-            $ch = curl_init("http://webapp15505.cloud683.configrapp.com/index.php?route=rest/manufacturer_admin/manufacturer");
+            $ch = curl_init(SELF::URL_SITE . "index.php?route=rest/manufacturer_admin/manufacturer");
             curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
             curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -216,6 +301,7 @@ class SiteExportaMarcasCommand extends Command
             }
             
         }
+        
         
         
     }
