f4179e7a5de882980492a8de6a4aa719cc64bf35
Luciano Reis <lucianoreis.dev@gmail.com>
2016-11-10 15:12:46 -0300

    [UPD] Correção labels formulários

--- resources/views/unidade-medida/form.blade.php
+++ resources/views/unidade-medida/form.blade.php
@@ -2,12 +2,12 @@
     //...
 ?>
 <div class="form-group">
-    <label for="produto" class="col-sm-2 control-label">{!! Form::label('Descrição :') !!}</label>
-    <div class="col-sm-4">{!! Form::text('unidademedida', null, ['class'=> 'form-control', 'id'=>'unidademedida']) !!}</div>
+    {!! Form::label('unidademedida', 'Descrição :', ['class'=>'col-sm-2 control-label']) !!}
+    <div class="col-sm-4">{!! Form::text('unidademedida', null, ['class'=> 'form-control', 'id'=>'unidademedida', 'required'=>'required']) !!}</div>
 </div>
 <div class="form-group">
-    <label for="produto" class="col-sm-2 control-label">{!! Form::label('Sigla:') !!}</label>
-    <div class="col-sm-1">{!! Form::text('sigla', null, ['class'=> 'form-control', 'id'=>'sigla']) !!}</div>
+    {!! Form::label('sigla', 'Sigla:', ['class'=>'col-sm-2 control-label']) !!}
+    <div class="col-sm-1">{!! Form::text('sigla', null, ['class'=> 'form-control', 'id'=>'sigla', 'required'=>'required']) !!}</div>
 </div>
 <hr>
 <div class="form-group">
@@ -28,7 +28,6 @@ $(document).ready(function() {
             }
         });
     });    
-    $('#sigla, #unidademedida').prop('required', true);
 });
 </script>
 @endsection
