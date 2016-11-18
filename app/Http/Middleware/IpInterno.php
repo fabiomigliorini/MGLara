59188ec995467f489ac6d49a10edcd5d3f130b6a
Luciano Reis <lucianoreis.dev@gmail.com>
2016-11-18 07:57:12 -0300

    [UPD] Todas as faixas de IPs internos

--- app/Http/Middleware/IpInterno.php
+++ app/Http/Middleware/IpInterno.php
@@ -20,16 +20,17 @@ class IpInterno
                 return redirect()->guest('auth/login');
             }
         }        
-        
         return $next($request);
-        
     }
     
     public function verificaIpInterno ($ip)
     {
         $reserved_ips = array( 
-            '2130706433'  => 2130706689, #  127.0.0.1 até  127.0.1.1
-            '3232235777'  => 3232236030, # 192.168.1.1 até 192.168.1.254
+            '2130706433' => 2130706689, #  127.0.0.1 até  127.0.1.1
+            '3232235777' => 3232236031, # 192.168.1.1 até 192.168.1.255
+            '3232236033' => 3232236287, # 192.168.2.1 até 192.168.2.255
+            '3232236289' => 3232236543, # 192.168.3.1 até 192.168.3.255
+            '3232236545' => 3232236799, # 192.168.4.1 até 192.168.4.255
         );
 
         $ip_long = sprintf('%u', ip2long($ip));
@@ -38,9 +39,9 @@ class IpInterno
         {
             if (($ip_long >= $ip_start) && ($ip_long <= $ip_end))
             {
-                return TRUE;
+                return TRUE; // IP interno
             }
         }
-        return FALSE;
+        return FALSE; // IP externo
     }    
 }
