5a0437841b0f0ae9684f384e347d0c29aec63e19
Fabio Migliorini <fabio@mgpapelaria.com.br>
2016-11-25 19:01:02 -0300

    [UPD] Relatorio Analise de Estoque

--- resources/views/layouts/print-portrait.blade.php
+++ resources/views/layouts/print-portrait.blade.php
@@ -5,6 +5,8 @@
     <meta name="author" content="MG Papelaria">
     <title>MG Papelaria</title>
     <link href="{{ URL::asset('public/css/print.css') }}" rel="stylesheet">
+    <link href="{{ URL::asset('public/vendor/font-awesome/css/font-awesome.min.css') }}" rel="stylesheet">
+    <script src="{{ URL::asset('public/vendor/jquery/2.1.1/jquery.min.js') }}"></script>
     <style type='text/css' media='all'>
         @media print {
             @page {
