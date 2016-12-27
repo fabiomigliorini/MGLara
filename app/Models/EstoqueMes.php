0611c2b1bd4b1dce164cec1d651d8d78890adca3
Fabio Migliorini <fabio@mgpapelaria.com.br>
2016-12-27 18:04:09 -0300

    [UPD] Ajuste Calculo Custo Medio / Voltado Fiscal por Ano

--- app/Models/EstoqueMes.php
+++ app/Models/EstoqueMes.php
@@ -115,7 +115,7 @@ class EstoqueMes extends MGModel
         $mes->year = $data->year;
         
         // Se for fiscal cria somente um mês por ano, dezembro
-        if ($fiscal && $mes->year <= 2015) {
+        if ($fiscal) {
             $mes->month = 12;
         }
         
