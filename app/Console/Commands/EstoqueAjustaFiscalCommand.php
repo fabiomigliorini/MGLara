aec0e87a9c56069098f1b0d8ca76518d611c142b
Fabio Migliorini <fabio@mgpapelaria.com.br>
2016-12-31 20:53:31 -0300

    [ADD] Geracao automatica notas de transferencia para saldo negativo

--- app/Console/Commands/EstoqueAjustaFiscalCommand.php
+++ app/Console/Commands/EstoqueAjustaFiscalCommand.php
@@ -11,6 +11,10 @@ use MGLara\Jobs\EstoqueCalculaCustoMedio;
 use MGLara\Models\EstoqueMes;
 use MGLara\Models\EstoqueMovimento;
 use MGLara\Models\EstoqueMovimentoTipo;
+use MGLara\Models\EstoqueLocal;
+use MGLara\Models\NotaFiscal;
+use MGLara\Models\NotaFiscalProdutoBarra;
+use MGLara\Models\ProdutoBarra;
 use Carbon\Carbon;
 
 class EstoqueAjustaFiscalCommand extends Command
@@ -53,6 +57,10 @@ class EstoqueAjustaFiscalCommand extends Command
                 $this->variacaoNegativa();
                 break;
 
+            case 'emite-transferencias':
+                $this->emiteTransferencias();
+                break;
+
             default:
                 $this->metodoDesconhecido();
                 break;
@@ -69,6 +77,178 @@ class EstoqueAjustaFiscalCommand extends Command
         $this->info('Métodos Disponíveis:');
         $this->line('');
         $this->line('- variacao-negativa: Ajusta estoque negativo da variação transferindo o saldo de outra variacao do mesmo produto, do mesmo local de estoque!');
+        $this->line('- emite-transferencias: Gera notas de transferencia de uma filial para outra a fim de corrigir o estoque negativo!');
+    }
+    
+    public function emiteTransferencias()
+    {
+        // Pega opcao do estoquelocal
+        $codestoquelocal = $this->option('codestoquelocal');
+
+        if (empty($codestoquelocal)) {
+            $this->line('');
+            $this->error('codestoquelocal não informado! Utilize --codestoquelocal=?');
+            $this->line('');
+            return;
+        }
+        
+        // Instancia Estoque Local
+        $el = EstoqueLocal::findOrFail($codestoquelocal);
+        
+        // Busca saldos negativos do estoquelocal
+        $sql = "
+            select p.codproduto, p.produto, elpv.codestoquelocal, elpv.codestoquelocalprodutovariacao, el.estoquelocal, es.customedio, pv.codprodutovariacao, pv.variacao, es.saldoquantidade, es.saldovalor, es.codestoquesaldo, (select mes.codestoquemes from tblestoquemes mes where mes.codestoquesaldo = es.codestoquesaldo order by mes desc limit 1) as codestoquemes
+            from tblproduto p
+            inner join tblprodutovariacao pv on (pv.codproduto = p.codproduto)
+            inner join tblestoquelocalprodutovariacao elpv on (elpv.codprodutovariacao = pv.codprodutovariacao)
+            inner join tblestoquelocal el on (el.codestoquelocal = elpv.codestoquelocal)
+            inner join tblfilial f on (f.codfilial = el.codfilial)
+            inner join tblestoquesaldo es on (es.codestoquelocalprodutovariacao = elpv.codestoquelocalprodutovariacao and es.fiscal = true)
+            where elpv.codestoquelocal = $codestoquelocal
+            and coalesce(es.saldoquantidade, 0) < 0
+            order by p.codproduto, saldoquantidade
+        ";
+        
+        $dados = DB::select($sql);
+        
+        // Pergunta se deseja continuar
+        if (!$this->confirm(sizeof($dados) . ' registros com saldo negativo encontrados! Continuar [y|N]')) {
+            return;
+        }        
+
+        // Percorre negativos
+        foreach ($dados as $negativo) {
+            
+            // Mostra registro
+            $this->line("{$negativo->codproduto} - {$negativo->produto} - {$negativo->variacao} - {$negativo->saldoquantidade}");
+            
+            // Busca alternativas da mesma variacao
+            $sql = "
+                select coalesce(pv.variacao, '{ Sem Variacao }') as variacao, pv.codprodutovariacao, elpv.codestoquelocal, es.codestoquesaldo, es.saldoquantidade, es.customedio, es.codestoquelocalprodutovariacao
+                from tblprodutovariacao pv
+                inner join tblestoquelocalprodutovariacao elpv on (elpv.codprodutovariacao = pv.codprodutovariacao)
+                inner join tblestoquesaldo es on (es.codestoquelocalprodutovariacao = elpv.codestoquelocalprodutovariacao and es.fiscal = true)
+                where pv.codprodutovariacao = {$negativo->codprodutovariacao}
+                and es.codestoquelocalprodutovariacao != {$negativo->codestoquelocalprodutovariacao}
+                and es.saldoquantidade > 0
+                order by elpv.codestoquelocal, es.saldoquantidade DESC, pv.variacao ASC NULLS FIRST
+            ";
+            
+            $alternativas = DB::select($sql);
+            
+            // Soma quantidade disponivel das alternativas
+            $qtd_alternativas = 0;
+            foreach ($alternativas as $alternativa) {
+                if (isset($cache_saldos[$alternativa->codestoquesaldo])) {
+                    $qtd_alternativas += $cache_saldos[$alternativa->codestoquesaldo];
+                } else {
+                    $qtd_alternativas += $alternativa->saldoquantidade;
+                }
+            }
+            
+            // se a quantidade disponivel for menor que o saldo negativo 
+            // busca somente pelo produto, independente da variacao
+            $saldo = abs($negativo->saldoquantidade);
+            if ($qtd_alternativas < $saldo) {
+                
+                $sql = "
+                    select coalesce(pv.variacao, '{ Sem Variacao }') as variacao, pv.codprodutovariacao, elpv.codestoquelocal, es.codestoquesaldo, es.saldoquantidade, es.customedio, es.codestoquelocalprodutovariacao
+                    from tblprodutovariacao pv
+                    inner join tblestoquelocalprodutovariacao elpv on (elpv.codprodutovariacao = pv.codprodutovariacao)
+                    inner join tblestoquesaldo es on (es.codestoquelocalprodutovariacao = elpv.codestoquelocalprodutovariacao and es.fiscal = true)
+                    where pv.codproduto = {$negativo->codproduto}
+                    and es.codestoquelocalprodutovariacao != {$negativo->codestoquelocalprodutovariacao}
+                    and es.saldoquantidade > 0
+                    order by elpv.codestoquelocal, es.saldoquantidade DESC, pv.variacao ASC NULLS FIRST
+                ";
+
+                $alternativas = DB::select($sql);
+                
+            } 
+            
+            // Percorre alternativas
+            $i=0;
+            while ($saldo > 0 && ($i <= (sizeof($alternativas) -1))) {
+                
+                // faz um cache do saldo, controlando quanto ja foi utilizado
+                if (!isset($cache_saldos[$alternativas[$i]->codestoquesaldo])) {
+                    $cache_saldos[$alternativas[$i]->codestoquesaldo] = $alternativas[$i]->saldoquantidade;
+                }
+                
+                // utiliza a menor quantidade entre o saldo disponivel e o negativo
+                $quantidade = min([$saldo, $cache_saldos[$alternativas[$i]->codestoquesaldo]]);
+                
+                // se tinha algo disponivel
+                if ($quantidade > 0) {
+                    
+                    // se a nota ja tiver mais de 500 itens, não deixa utilizar mais ela
+                    if (isset($nfs[$alternativas[$i]->codestoquelocal])) {
+                        if ($nfs[$alternativas[$i]->codestoquelocal]->NotaFiscalProdutoBarraS()->count() >= 500) {
+                            unset($nfs[$alternativas[$i]->codestoquelocal]);
+                        }
+                    }
+                    
+                    // cria nota fiscal
+                    if (!isset($nfs[$alternativas[$i]->codestoquelocal])) {
+                        $nfs[$alternativas[$i]->codestoquelocal] = new NotaFiscal;
+                        $nfs[$alternativas[$i]->codestoquelocal]->codestoquelocal = $alternativas[$i]->codestoquelocal;
+                        $nfs[$alternativas[$i]->codestoquelocal]->codfilial = $nfs[$alternativas[$i]->codestoquelocal]->EstoqueLocal->codfilial;
+                        $nfs[$alternativas[$i]->codestoquelocal]->codpessoa = $el->Filial->codpessoa;
+                        $nfs[$alternativas[$i]->codestoquelocal]->modelo = NotaFiscal::MODELO_NFE;
+                        $nfs[$alternativas[$i]->codestoquelocal]->codnaturezaoperacao = 15; //Transferencia de Saida
+                        $nfs[$alternativas[$i]->codestoquelocal]->serie = 1;
+                        $nfs[$alternativas[$i]->codestoquelocal]->numero = 0;
+                        $nfs[$alternativas[$i]->codestoquelocal]->emitida = true;
+                        $nfs[$alternativas[$i]->codestoquelocal]->emissao = new Carbon('now'); 
+                        $nfs[$alternativas[$i]->codestoquelocal]->saida = $nfs[$alternativas[$i]->codestoquelocal]->emissao;
+                        $nfs[$alternativas[$i]->codestoquelocal]->save();
+                        $geradas[$nfs[$alternativas[$i]->codestoquelocal]->codnotafiscal] = 0;
+                    }
+                    
+                    // pega o produto barra
+                    if (!$pb = ProdutoBarra::where('codprodutovariacao', '=', $alternativas[$i]->codprodutovariacao)->whereNull('codprodutoembalagem')->first()) {
+                        if (!$pb = ProdutoBarra::where('codprodutovariacao', '=', $alternativas[$i]->codprodutovariacao)->first()) {
+                            continue;
+                        }
+                    }
+                    
+                    // cria o item da nota
+                    $nfpb = new NotaFiscalProdutoBarra;
+                    $nfpb->codprodutobarra = $pb->codprodutobarra;
+                    $nfpb->codnotafiscal = $nfs[$alternativas[$i]->codestoquelocal]->codnotafiscal;
+                    $nfpb->quantidade = $quantidade;
+                    $nfpb->valorunitario = $alternativas[$i]->customedio;
+                    if ($nfpb->valorunitario == 0) {
+                        $nfpb->valorunitario = $negativo->customedio;
+                    }
+                    if ($nfpb->valorunitario == 0) {
+                        $nfpb->valorunitario = $pb->Produto->preco * 0.7;
+                    }
+                    if (!empty($pb->codprodutoembalagem)) {
+                        $nfpb->quantidade /= $pb->ProdutoEmbalagem->quantidade;
+                        $nfpb->valorunitario *= $pb->ProdutoEmbalagem->quantidade;
+                    }
+                    $nfpb->valortotal = $nfpb->quantidade * $nfpb->valorunitario;
+                    $nfpb->calculaTributacao();
+                    $nfpb->save();
+                    
+                    // incrementa a quantidade de itens da nota gerada
+                    $geradas[$nfs[$alternativas[$i]->codestoquelocal]->codnotafiscal]++;
+                    
+                    // diminui a quantidade do saldo e do cache
+                    $saldo -= $quantidade;
+                    $cache_saldos[$alternativas[$i]->codestoquesaldo] -= $quantidade;
+                }
+                
+                // proxima alternativa
+                $i++;
+            }
+        }
+            
+        // lista notas geradas e a quantidade de notas
+        foreach ($geradas as $codnotafiscal => $itens) {
+            $this->line("Gerada NF $codnotafiscal com $itens itens");
+        }
     }
     
     public function variacaoNegativa()
