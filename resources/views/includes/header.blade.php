ececf998b93c86a21f545cf6beffa22271f2de76
candido <candido@candido-pc>
2017-01-21 12:33:10 -0300

    [UPD] Menu

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -88,7 +88,8 @@
                 <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Financeiro <span class="caret"></span></a>
                     <ul class="dropdown-menu">
-                        <li><a href="{{ url('usuario') }}">Cheques</a></li>
+                        <li><a href="{{ url('cheque') }}">Cheques</a></li>
+                        <li><a href="{{ url('cheque-motivo-devolucao') }}">Motivos de Devolução de Cheques</a></li>
                     </ul>
                 </li>
                 <li class="dropdown">
