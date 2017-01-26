fd8b2f7e080a114d52cfdd9e5b44be6e96dfd5cb
Fabio Migliorini <fabio@mgpapelaria.com.br>
2017-01-26 17:53:12 -0300

    [UPD] Ordena por preco

--- app/Console/Commands/EstoqueAjustaFiscalCommand.php
+++ app/Console/Commands/EstoqueAjustaFiscalCommand.php
@@ -420,7 +420,7 @@ class EstoqueAjustaFiscalCommand extends Command
             inner join tblfilial f on (f.codfilial = el.codfilial)
             inner join tblncm n on (n.codncm = p.codncm)
             where em.saldoquantidade < 0
-            order by em.mes, n.ncm, p.preco DESC, p.produto, pv.variacao nulls first, elpv.codestoquelocal
+            order by em.mes, n.ncm, p.preco DESC, p.produto, elpv.codestoquelocal, pv.variacao nulls first
             limit 1
             ";
 
@@ -667,7 +667,7 @@ class EstoqueAjustaFiscalCommand extends Command
             inner join tblestoquelocal el on (el.codestoquelocal = elpv.codestoquelocal)
             inner join tblncm n on (n.codncm = p.codncm)
             where em.saldoquantidade < 0
-            order by em.mes, n.ncm, p.produto, pv.variacao nulls first, elpv.codestoquelocal
+            order by em.mes, n.ncm, p.preco DESC, p.produto, elpv.codestoquelocal, pv.variacao nulls first
             limit 1
             ";
 
