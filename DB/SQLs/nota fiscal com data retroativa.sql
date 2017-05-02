1ae94592210d9c1bdd22609c78e50d4d4816d29f
Fabio Migliorini <fabio@mgpapelaria.com.br>
2017-05-01 23:16:48 -0400

    [UPD] SQLs

--- DB/SQLs/nota fiscal com data retroativa.sql
+++ DB/SQLs/nota fiscal com data retroativa.sql
@@ -2,7 +2,8 @@
 set numero = nextval('tblnotafiscal_numero_' || cast(tblnotafiscal.codfilial as varchar) || '_' || cast(tblnotafiscal.serie as varchar) || '_' || cast(tblnotafiscal.modelo as varchar) || '_seq')
 , emissao = '2017-04-30 22:00'
 , saida  = '2017-04-30 22:00'
-where tblnotafiscal.codnotafiscal between 463227 and 463227
+where tblnotafiscal.codnotafiscal between 00529645 and 00529647
 and tblnotafiscal.numero = 0
 
 
+update tblnotafiscalprodutobarra set valortotal = quantidade * valorunitario where codnotafiscal between 00529645 and 00529647
