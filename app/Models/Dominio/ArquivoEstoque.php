8f83540ffaf80ddfddf626e47e0c763e04278e64
root <root@netuno.mgpapelaria.com.br>
2020-03-19 17:46:34 -0400

    [UPD] Estoque Dominio 2019

--- app/Models/Dominio/ArquivoEstoque.php
+++ app/Models/Dominio/ArquivoEstoque.php
@@ -42,6 +42,7 @@ class ArquivoEstoque extends Arquivo
 
         $dataSaldo = $this->_mes->modify('last day of this month');
 
+	/*
         $sql = "
             select
 	        p.codproduto
@@ -77,6 +78,51 @@ class ArquivoEstoque extends Arquivo
             AND fiscal.saldoquantidade != 0
             order by 7 desc, p.produto, p.codproduto
         ";
+	 */
+
+	$sql = "
+	    select
+		p.codproduto
+		, p.produto
+		, p.inativo
+		, p.codtipoproduto
+		, p.preco
+		, um.sigla
+		, n.ncm
+		, fiscal.saldoquantidade
+		, cus.custoultimaentrada as customedio
+		, fiscal.saldoquantidade * cus.custoultimaentrada as saldovalor
+		--, fiscal.customedio
+		--, fiscal.saldovalor
+		, p.preco / case when cus.custoultimaentrada != 0 then cus.custoultimaentrada else null end as markup
+	    from tblproduto p
+	    left join tblunidademedida um on (um.codunidademedida = p.codunidademedida)
+	    left join (
+		select 
+			pv.codproduto
+			, sum(em.saldoquantidade) as saldoquantidade
+			, sum(em.saldovalor) as saldovalor
+			, sum(em.saldovalor) / case when sum(em.saldoquantidade) !=0 then sum(em.saldoquantidade) else null end as customedio
+		from tblestoquelocalprodutovariacao elpv
+		inner join tblprodutovariacao pv on (pv.codprodutovariacao = elpv.codprodutovariacao)
+		inner join tblestoquelocal el on (el.codestoquelocal = elpv.codestoquelocal)
+		inner join tblfilial f on (f.codfilial = el.codfilial)
+		inner join tblestoquesaldo es on (es.codestoquelocalprodutovariacao = elpv.codestoquelocalprodutovariacao and es.fiscal = true)
+		inner join tblestoquemes em on (em.codestoquemes = (select em2.codestoquemes from tblestoquemes em2 where em2.codestoquesaldo = es.codestoquesaldo and em2.mes <= :mes order by mes desc limit 1))
+		where f.codfilial = :codfilial
+	      group by pv.codproduto
+		) fiscal on (fiscal.codproduto = p.codproduto)
+            left join (
+                select ci.codproduto, max(custoultimaentrada) as custoultimaentrada
+                from tblestoque2019creditoicms ci
+                group by ci.codproduto
+            ) cus on (cus.codproduto = p.codproduto)
+	    left join tblncm n on (n.codncm = p.codncm)
+	    where p.codtipoproduto = 0
+	    AND fiscal.saldoquantidade != 0
+	    order by p.codproduto, p.produto
+
+	";
 
 	$params = [
             'mes' => $dataSaldo,
