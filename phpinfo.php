f091a7630a8cb32175dea0270e18f5ff21e1b2b9
Fabio Migliorini <fabio@mgpapelaria.com.br>
2022-01-07 13:46:23 -0400

    [FIX] Timezone

--- phpinfo.php
+++ phpinfo.php
@@ -1,3 +1,8 @@
 <?php
+$dateTime = new DateTime();
+$dateTime->setTimeZone(new DateTimeZone('America/Cuiaba'));
+echo $dateTime->format('d/m/Y H:i:s e P');
+//var_dump($dateTime);
+
 echo phpinfo();
 ?>
