169c0398368918c492e799f32a53bfb8bf8a8fa8
Fabio Migliorini <fabio@mgpapelaria.com.br>
2023-10-10 12:13:09 -0400

    [FIX] Https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace MGLara\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +15,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if(env('FORCE_HTTPS',false)) { // Default value should be false for local server
+            URL::forceSchema('https');
+        }
     }
 
     /**
