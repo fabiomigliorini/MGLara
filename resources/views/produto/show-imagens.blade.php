b0b25c8711ae9186071ee6566dffe60f8ddc6e37
Fabio Migliorini <fabio@mgpapelaria.com.br>
2023-11-23 16:08:28 -0400

    [FIX] Indicadores Carousel estourando. Ex Produto #000374

--- resources/views/produto/show-imagens.blade.php
+++ resources/views/produto/show-imagens.blade.php
@@ -9,11 +9,13 @@
         </a>
     </p>
     <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+        <!--
         <ol class="carousel-indicators">
             @for ($i = 0; $i < sizeof($imagens); $i++)
                 <li data-target="#myCarousel" data-slide-to="{{ $i }}" class="{{ ($i == 0)?'active':'' }}"></li>
             @endfor
         </ol>
+        -->
         <div class="carousel-inner" role="listbox">
             @if (sizeof($imagens) == 0)
                 <div class="item text-center {{ ($i == 0)?'active':'' }}">
@@ -26,11 +28,10 @@
                         <img src="<?php echo URL::asset('public/imagens/'.$imagem->observacoes);?>" id="{{$imagem->codimagem}}" style='margin: 0 auto;'>
                     </a>
                     <div class="carousel-caption">
-                        <p>{{ $imagem->observacoes }}</p>
-                        <p>
-                            <a href='{{ url("imagem/produto/$model->codproduto?imagem={$imagem->codimagem}") }}'><i class="glyphicon glyphicon-pencil"></i></a>
-                            <a href="{{ url("imagem/produto/{$model->codproduto}/delete?imagem={$imagem->codimagem}") }}" data-excluir data-pergunta="Tem certeza que deseja excluir a imagem '{{ $imagem->observacoes }}'?" data-after-delete="recarregaDiv('div-imagens');"><i class="glyphicon glyphicon-trash"></i></a>
-                        </p>
+                        <div class="btn-group" role="group">
+                            <a class="btn btn-primary" href='{{ url("imagem/produto/$model->codproduto?imagem={$imagem->codimagem}") }}'><i class="glyphicon glyphicon-pencil"></i></a>
+                            <a class="btn btn-primary" href="{{ url("imagem/produto/{$model->codproduto}/delete?imagem={$imagem->codimagem}") }}" data-excluir data-pergunta="Tem certeza que deseja excluir a imagem '{{ $imagem->observacoes }}'?" data-after-delete="recarregaDiv('div-imagens');"><i class="glyphicon glyphicon-trash"></i></a>
+                        </div>
                     </div>
                 </div>
             @endforeach
